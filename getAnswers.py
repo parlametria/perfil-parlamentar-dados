6b700d68dc8071dcc4711123d7a628cdb02311a5
silveiraluiza <luiza.silveira@ccc.ufcg.edu.br>
2018-08-25 01:06:25 -0300

    Modifica para pegar as respostas via bulk

--- getAnswers.py
+++ getAnswers.py
@@ -22,43 +22,14 @@ s.headers.update({
 #with open('data1.json', 'w') as file:
 #    file.write(data)
 
-url = "https://api.surveymonkey.com/v3/surveys/%s/responses/" % (keys.survey_id)
+url = "https://api.surveymonkey.com/v3/surveys/%s/responses/bulk" % (keys.survey_id)
 
-def getIds(url, listaids):
-    request = s.get(url)
-    temp = json.loads(request.text)
-    ids = listaids 
+print("iniciando request")
+request = s.get(url)
+temp = json.loads(request.text)
+data = json.dumps(temp,sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
 
-    for (k, v) in temp.items():
-        if k == "data":
-            for i in v:
-                ids.append(i["id"])
-        
-        if k == "links":
-            if url == "https://api.surveymonkey.net/v3/surveys/155811540/responses/?page=8&per_page=50":
-                return(ids)
-            else:
-                return getIds(v["next"], ids)
-
-empty = []
-print("inicia get dos ids das respostas")
-ids = getIds(url, empty)
-print("inicia get de respostas")
-data = "["
-
-for i in range(0,len(ids)):
-    url = "https://api.surveymonkey.com/v3/surveys/%s/responses/%s/details" %(keys.survey_id, ids[i])
-    request = s.get(url)
-    temp = json.loads(request.text)
-    data += json.dumps(temp,sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
-    if(i >= len(ids)):
-        break
-    else:
-        data += ", "
-
-data += "]"
-print("salvando os dados...")        
-with open('respostas.json', 'w') as file:
+with open('responses.json', 'w') as file:
     file.write(data)
 
 print("finalizado")
