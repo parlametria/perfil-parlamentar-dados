cf21f27cb86e869120214762375960008743d3f3
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2018-08-26 12:37:35 -0300

    Dá nome pythonico ao script e adiciona placeholder keys.py

--- monkey2json.py
+++ monkey2json.py
@@ -9,19 +9,6 @@ s.headers.update({
   "Content-Type": "application/json"
 })
 
-# Script para pegar as respostas das surveys
-#data = ""
-#temp = ""
-# lista contém o id das páginas
-#for i in ['37769727', '38341527', '38341716', '38341742', '38341818']:
-#    url = "https://api.surveymonkey.com/v3/surveys/%s/pages/%s/questions" % (keys.survey_id,i)
-#    request = s.get(url)
-#    temp = json.loads(request.text)
-#    data += json.dumps(temp,sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
-    
-#with open('data1.json', 'w') as file:
-#    file.write(data)
-
 url = "https://api.surveymonkey.com/v3/surveys/%s/responses/bulk" % (keys.survey_id)
 
 print("iniciando request")
@@ -33,4 +20,3 @@ with open('responses.json', 'w') as file:
     file.write(data)
 
 print("finalizado")
-
