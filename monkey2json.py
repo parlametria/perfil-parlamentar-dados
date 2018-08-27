3052d757943b82c3e0761d5fd76590f0c5b134e0
silveiraluiza <luiza.silveira@ccc.ufcg.edu.br>
2018-08-27 02:37:13 -0300

    Adiciona payload

--- monkey2json.py
+++ monkey2json.py
@@ -8,11 +8,12 @@ s.headers.update({
   "Authorization": "Bearer %s" % keys.YOUR_ACCESS_TOKEN,
   "Content-Type": "application/json"
 })
+payload = {'per_page': 100}
 
 url = "https://api.surveymonkey.com/v3/surveys/%s/responses/bulk" % (keys.survey_id)
 
 print("iniciando request")
-request = s.get(url)
+request = s.get(url, params=payload)
 temp = json.loads(request.text)
 data = json.dumps(temp,sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
 
