9c923a759e1e43124d434fb7511d5d80301e9914
silveiraluiza <luiza.silveira@ccc.ufcg.edu.br>
2018-08-30 18:39:38 -0300

    Elimina a  duplicação das respostas

--- monkey2json.py
+++ monkey2json.py
@@ -2,6 +2,8 @@
 import keys
 import requests
 import json
+import datetime
+from datetime import timedelta,datetime
 
 NAO_RESPONDEU = 0
 
@@ -125,6 +127,30 @@ def request_page(page_url, data, data_slim):
         data_slim += "]"
         return data, data_slim
 
+def compara_candidatos(candidatos, data_slim_clone, data_slim):
+    lista_candidatos = []
+    for candidato in candidatos:
+        lista_candidatos.append(candidato["cpf"])
+
+    lista_resultados = []
+    for resultado in data_slim_clone:
+        if resultado["cpf"] != None:
+            if len(resultado["cpf"]) < 11:
+                resultado["cpf"] = (11 - len(resultado["cpf"]))*"0" + str(resultado["cpf"])
+            lista_resultados.append(resultado["cpf"])
+
+    lista_final =  [x for x in lista_candidatos if x not in lista_resultados]
+
+    data_slim = data_slim[:-1]
+    data_slim += ", "
+    for candidato in candidatos:
+        if candidato["cpf"] in lista_final:
+            data_slim += json.dumps(candidato, sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
+            data_slim += ", "
+
+    data_slim = data_slim[:-2]
+    data_slim += "]"
+    return data_slim
 
 s = requests.Session()
 s.headers.update({
@@ -142,67 +168,57 @@ data, data_slim = request_page(url,data, data_slim)
 
 candidatos = get_todos_candidatos()
 
-with open('respostas_slim.json') as f:
-    data_old = json.load(f)
-
 with open('respostas_novo.json', 'w') as file:
     file.write(data_slim)
-
-#with open('respostas.json', 'w') as file:
-#    file.write(data)
 
 print("Comparando csv de candidatos com os resultados do SM") 
 
 with open('respostas_novo.json') as f:
-    data_slim_temp = json.load(f)
+    data_slim_clone = json.load(f)
 
-lista_candidatos = []
-for candidato in candidatos:
-    lista_candidatos.append(candidato["cpf"])
-
-lista_resultados = []
-for resultado in data_slim_temp:
-    if resultado["cpf"] != None:
-        if len(resultado["cpf"]) < 11:
-            resultado["cpf"] = (11 - len(resultado["cpf"]))*"0" + str(resultado["cpf"])
-        lista_resultados.append(resultado["cpf"])
-
-lista_final =  [x for x in lista_candidatos if x not in lista_resultados]
-
-data_slim = data_slim[:-1]
-data_slim += ", "
-for candidato in candidatos:
-    if candidato["cpf"] in lista_final:
-        data_slim += json.dumps(candidato, sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
-        data_slim += ", "
-
-data_slim = data_slim[:-2]
-data_slim += "]"
-
-lista_cpf = []
-lista_tempo_final = []
-for cand in data_old:
-    lista_cpf.append(cand["cpf"])
-    lista_tempo_final.append(cand["date_modified"])
-
-alteracoes = []
+data_slim = compara_candidatos(candidatos,data_slim_clone, data_slim)
 
 with open('respostas_novo.json', 'w') as file:
     file.write(data_slim)
 
+print("Procurando alterações em respostas")
+
+with open('respostas_slim.json') as f:
+    data_old = json.load(f)
+
 with open('respostas_novo.json') as f:
     data_slim = json.load(f)
 
+dados_alterados = json.dumps(data_old, sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
+
+lista_cpf = []
+lista_tempo_final = []
+old_unique = { each['cpf'] : each for each in data_old }.values()
+
+alteracoes = []
 for cand in data_slim:
-    for n in range(len(lista_cpf)):
-        if cand["cpf"] == lista_cpf[n]:
-            if cand["date_modified"] != lista_tempo_final[n]:
+    for c in old_unique:
+        if cand["cpf"] == c["cpf"]:
+            if c["date_modified"] == "":
+                date_old = datetime.strptime("0001-01-01T00:00:00", "%Y-%m-%dT%H:%M:%S")
+            else:
+                strs = c["date_modified"]
+                strs = strs[::-1].replace(':','',1)[::-1]
+                date_old = datetime.strptime(strs[:-5], "%Y-%m-%dT%H:%M:%S")
+            if cand["date_modified"] == "":
+                date_new = datetime.strptime("0001-01-01T00:00:00", "%Y-%m-%dT%H:%M:%S")
+            else:
+                strs = cand["date_modified"]
+                strs = strs[::-1].replace(':','',1)[::-1]
+                date_new  = datetime.strptime(strs[:-5], "%Y-%m-%dT%H:%M:%S") 
+            
+            if date_new > date_old:
                 alteracoes.append(cand)
 
-dados_alterados = json.dumps(data_old, sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
 if len(alteracoes) > 0:
     print("Existem alterações")
     dados_alterados = dados_alterados[:-1]
+    dados_alterados += ", "
     for candidato in alteracoes:
         print(candidato)
         dados_alterados += json.dumps(candidato, sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
@@ -212,6 +228,8 @@ if len(alteracoes) > 0:
     print("Salvando os dados")
     with open('respostas_slim.json', 'w') as file:
         file.write(dados_alterados)
+else: 
+    print("Não Existem alterações")
 
 
 
