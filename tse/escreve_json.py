deb5094babc2334136608f254a1a7a0927f20cde
silveiraluiza <luiza.silveira@ccc.ufcg.edu.br>
2018-08-31 14:31:40 -0300

    Remove erro no json

--- tse/escreve_json.py
+++ tse/escreve_json.py
@@ -3,6 +3,10 @@ import csv
 import json
 import codecs
 
+def row_count(filename):
+    with open(filename) as in_file:
+        return sum(1 for _ in in_file)
+
 
 csvfile = open('./dados tratados/candidatos.csv', "r", encoding="latin-1")
 jsonfile = open('candidatos.json', 'w', encoding= 'latin-1')
@@ -11,19 +15,17 @@ names = ("uf","estado","nome_candidato","nome_urna",
                                "nome_social","email","tipo_agremiacao","num_partido","sg_partido",
                                "partido", "nome_coligacao","composicao_coligacao","idade_posse",
                                "genero","grau_instrucao","raca","ocupacao","cpf","nome_exibicao")
-import csv
-
-import json
-
 
 reader = csv.DictReader( csvfile, names)
 
 jsonfile.write('[')
+last_line_number = 8370
 
 for row in reader:
-
     json.dump(row, jsonfile, ensure_ascii=False)
-
-    jsonfile.write(',\n')
+    if last_line_number == reader.line_num:
+        print("Última linha") 
+    else:
+        jsonfile.write(',\n')
 
 jsonfile.write(']')
