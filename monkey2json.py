8d20c064f12956f243293dcd07cebf46fcc068de
silveiraluiza <luiza.silveira@ccc.ufcg.edu.br>
2018-08-31 14:32:09 -0300

    Respostas é um dicionário ao invés de uma tupla

--- monkey2json.py
+++ monkey2json.py
@@ -11,6 +11,8 @@ def get_todos_candidatos():
     with open('./tse/candidatos.json') as f:
         candidatos = json.load(f)
     
+    candidatos[:] = [d for d in candidatos if d.get('cpf') != "cpf"]
+
     for elem in candidatos:
         elem.pop('ocupacao', None)
         elem.pop('estado', None)
@@ -25,13 +27,10 @@ def get_todos_candidatos():
         elem.pop('idade_posse', None)
         elem.pop('genero', None)
         elem.pop('grau_instrucao', None)
-        elem["respostas"] = {"0":0, "1":0,"2":0,"3":0, "4":0, "5":0, 
-        "6":0, "7":0, "8":0, "9":0, "10":0, "11":0, "12":0, "13":0, "14":0, "15":0, "16":0,
-        "17":0, "18":0, "19":0, '20':0, "21":0, "22":0, "23":0, "24":0, "25":0, "26":0, "27":0,
-        "28":0, "29":0, "30":0, "31":0, "32":0, "33":0, "34":0, "35":0, "36":0, "37":0, "38":0,
-        "39":0, "40":0, "41":0, "42":0, "43":0, "44":0, "45":0},
-        elem["date_modified"] = ""
-
+        elem['respostas'] = {}
+        elem['date_modified'] = ""
+        for i in range(46):
+            elem['respostas'][str(i)] = 0
     return candidatos
             
 def change_candidato(json_candidato):
@@ -145,6 +144,10 @@ def compara_candidatos(candidatos, data_slim_clone, data_slim):
 
     lista_final =  [x for x in lista_candidatos if x not in lista_resultados]
 
+    print(len(lista_candidatos))
+    print(len(lista_resultados))
+    print(len(lista_final))
+    
     data_slim = data_slim[:-1]
     data_slim += ", "
     for candidato in candidatos:
