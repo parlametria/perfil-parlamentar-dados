9d393729d6c675019728995ec00984b6c1be053e
silveiraluiza <luiza.silveira@ccc.ufcg.edu.br>
2018-09-06 18:24:15 -0300

    Baixa fotos dos candidatos

--- tse/request_file.py
+++ tse/request_file.py
@@ -16,3 +16,8 @@ createFolder('./dados tratados/')
 r = requests.get("http://agencia.tse.jus.br/estatistica/sead/odsele/consulta_cand/consulta_cand_2018.zip")
 z = zipfile.ZipFile(io.BytesIO(r.content))
 z.extractall("./dados candidatos/")
+
+
+r = requests.get("https://drive.google.com/uc?id=1J2GZm379XB4zKSboqWrjc9etLcf5NrQV&export=download")
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall("../")
