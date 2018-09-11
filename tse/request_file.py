0171d3aa0d0eee0d86f9b780423138b653caee23
silveiraluiza <luiza.silveira@ccc.ufcg.edu.br>
2018-09-11 14:52:50 -0300

    Cria pasta de dados no script

--- tse/request_file.py
+++ tse/request_file.py
@@ -12,6 +12,7 @@ def createFolder(directory):
 
 createFolder('./dados candidatos/')
 createFolder('./dados tratados/')
+createFolder('../dados/')
 
 r = requests.get("http://agencia.tse.jus.br/estatistica/sead/odsele/consulta_cand/consulta_cand_2018.zip")
 z = zipfile.ZipFile(io.BytesIO(r.content))
