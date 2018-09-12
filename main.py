e83a9b9f648a5656462ddda2c55f91d7f1646bb7
silveiraluiza <luiza.silveira@ccc.ufcg.edu.br>
2018-09-12 13:59:38 -0300

    Atualiza coleção de candidatos

--- main.py
+++ main.py
@@ -4,47 +4,19 @@ from pymongo import MongoClient, ReturnDocument
 
 start_time = time.time()
 
-# Dados TSE
-request_file.main()
-subprocess.call ("/usr/bin/Rscript --vanilla ./tse/cria_planilha_tratada.R", shell=True)
-escreve_json.main()
 
-# Survey Monkey
-verifica_candidatos.main()
-keys4answers2json.main()
-questions2json.main()
-monkey2json.main()
 
 # Conectando ao banco de validação
 client = MongoClient(keys.VALIDACAO_URI)
 db = client.heroku_j4qrssbw
 collection = db.respostas
 
-jsonObj =  monkey2json.recupera_dados("./dados/respostas_novo.json")
-
-i = 0
-for candidato in jsonObj:
-    collection.find_one_and_update(
-   { "cpf" : candidato["cpf"] },
-   { "$set": { "respostas" : candidato["respostas"], "respondeu": candidato["respondeu"],
-    "recebeu": candidato["recebeu"], "date_created": candidato["date_created"], 
-    "date_modified": candidato["date_modified"] }}, upsert=True)
-    i += 1
-    print(i)
 
 
-collection = db.mudancas
-
-jsonObj =  monkey2json.recupera_dados("./dados/mudancas.json")
-i = 0
-for alteracao in jsonObj:
-    collection.find_one_and_update(
-   { "data" : alteracao["data"], "alteracoes": alteracao["alteracoes"] },
-   { "$set": { "alteracoes": alteracao["alteracoes"] }}, upsert=True)
-    i += 1
-    print(i)
-
-
+collection = db.candidatos
+jsonObj =  monkey2json.recupera_dados("./tse/candidatos.json")
+collection.drop()
+collection.insert_many(jsonObj)
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
