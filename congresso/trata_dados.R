1d823b56271fbd7a6d69948beecbe7d8906eb5bc
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-09-27 20:14:34 -0300

    Pega dados de todos os candidatos de 2018 (falta de antes)

--- congresso/trata_dados.R
+++ congresso/trata_dados.R
@@ -4,26 +4,38 @@ library(rcongresso)
 library(readr)
 library(jsonlite)
 library(tidyverse)
+library(congressbr)
 
 votacoes <- read_csv("~/Documentos/vozativa-monkey-ui/congresso/TabelaAuxVotacoes .csv")
 candidatos_2018 <- read_csv("~/Documentos/vozativa-monkey-ui/congresso/candidatos_2018.csv")
 
 ids_votacoes <- votacoes$id_votacao
 
-deputados <- fetch_deputado(itens = 520) %>% unique() 
+deputados20142018_id <- fetch_deputado(idLegislatura = 55, itens = -1)$id
 
-id_deputados <- deputados$id
+deputados20102014_id <- fetch_deputado(idLegislatura = 54, itens = -1)$id
 
-info_pessoais_dep <- fetch_deputado(id_deputados)
+info_pessoais_20102014 <- fetch_deputado(deputados20102014_id) %>% 
+  select(id, nomeCivil)
+
+info_pessoais_20142018 <- fetch_deputado(id_deputados) %>% 
+  select(id, nomeCivil)
 
 votos <- fetch_votos(votacoes$id_votacao) %>% 
-  select(id_votacao, parlamentar.nome, voto) %>%
+  select(id_votacao, parlamentar.nome, parlamentar.id, voto) %>%
   left_join(votacoes, by="id_votacao")  %>% 
-  left_join(info_pessoais_dep, by= c("parlamentar.nome" = "ultimoStatus.nomeEleitoral")) 
+  left_join(info_pessoais_20142018, by= c("parlamentar.id" = "id")) %>%
+  left_join(info_pessoais_20102014, by= c("parlamentar.id" = "id")) %>%
+  mutate(nomeCivil = ifelse(is.na(nomeCivil.x), nomeCivil.y, nomeCivil.x)) %>%
+  select(-nomeCivil.x, -nomeCivil.y)
 
-votos <- votos %>% select(id_votacao, parlamentar.nome, voto, cpf.y, nomeCivil, name, ultimoStatus.nomeEleitoral) 
-votosu <- na.omit(votos)
-votos %>% 
-  dplyr::group_by(parlamentar.nome) %>% 
+# Falta pegar os dados dos candidatos de 2010~2014
+votos_tratados <- candidatos_2018 %>%
+  select(cpf, name) %>%
+  right_join(votos, by=c("name"="nomeCivil"))
+
+votos_tratados %>% 
+  dplyr::group_by(cpf, name, parlamentar.nome, parlamentar.id) %>% 
   nest() %>% 
-  rename(projetos = data) %>% jsonlite::toJSON()
+  rename(projetos = data) %>% 
+  jsonlite::toJSON()
