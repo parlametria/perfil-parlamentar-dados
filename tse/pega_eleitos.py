dc06dc91fc114f75d22b7b62ef5dd2614a32c11d
luiza Carvalho <luiza.silveira@ccc.ufcg.edu.br>
2018-10-08 17:41:50 -0300

    Script que busca quem venceu as eleições de deputado federal é adicionado

---
+++ tse/pega_eleitos.py
@@ -0,0 +1,64 @@
+import keys
+import requests
+import json
+import datetime
+from datetime import timedelta,datetime, date
+import os, csv
+
+def escreve_dados(caminho,dados):
+    with open(caminho, 'w') as file:
+        file.write(dados)    
+
+def recupera_dados(caminho):
+    with open(caminho) as file:
+        dados = json.load(file)
+    return dados
+
+def busca_candidatos():
+    s = requests.Session()
+    s.encoding = 'ISO-8859-1'
+    estados = [
+        "AC",
+        "AL",
+        "AM",
+        "AP",
+        "BA",
+        "CE",
+        "DF",
+        "ES",
+        "GO",
+        "MA",
+        "MT",
+        "MS",
+        "MG",
+        "PA",
+        "PB",
+        "PR",
+        "PE",
+        "PI",
+        "RJ",
+        "RN",
+        "RO",
+        "RS",
+        "RR",
+        "SC",
+        "SE",
+        "SP",
+        "TO"
+    ]
+
+    data = "["
+    for e in estados:            
+        page_url = "http://interessados.divulgacao.tse.jus.br/2018/divulgacao/oficial/297/dadosdivweb/%s/%s-c0006-e000297-w.js" % (e.lower(), e.lower())
+        request = s.get(page_url)
+        request.encoding = 'UTF-8'
+        temp = json.loads(request.text)
+        data += json.dumps(temp,sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
+        data += ", "
+
+    data = data[:-2]
+    data += "]"
+    
+    escreve_dados("eleitos.json",data)
+
+busca_candidatos()
