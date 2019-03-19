1587cad32ec54a39bb94b0704546088c84777d17
gileadekelvin <gileadekelvin@gmail.com>
2019-03-19 16:04:49 -0300

    Adiciona funções que recuperam informações de comissões e de suas composições

---
+++ crawler/parlamentares/comissoes/constants/cargos.R
@@ -0,0 +1,7 @@
+# CONSTANTS
+
+.PRIMEIRO_PRESIDENTE = "PrimeiroVice-Presidente"
+.SEGUNDO_PRESIDENTE = "SegundoVice-Presidente"
+.TERCEIRO_PRESIDENTE = "TerceiroVice-Presidente"
+.TITULAR = "Titular"
+.SUPLENTE = "Suplente"
