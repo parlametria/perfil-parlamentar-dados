034aab5134acc7435af85e74163793373ab55b2a
Hadrizia <hadrizia@gmail.com>
2019-04-24 13:54:17 -0300

    Adicionada migrations, criação e importação dos dados de mandatos

--- bd/scripts/create_table_bd_vozativa.sql
+++ bd/scripts/create_table_bd_vozativa.sql
@@ -107,3 +107,14 @@ CREATE TABLE IF NOT EXISTS "composicao_comissoes" (
     "situacao" VARCHAR(255),
     PRIMARY KEY("comissao_id", "parlamentar_cpf")
 );
+
+CREATE TABLE IF NOT EXISTS "mandatos" (
+    "id_parlamentar_voz" VARCHAR(40) REFERENCES "parlamentares" ("id_parlamentar_voz") ON DELETE CASCADE ON UPDATE CASCADE,
+    "id_legislatura" INTEGER,
+    "data_inicio" DATE,
+    "data_fim" DATE,
+    "situacao" VARCHAR(255),
+    "cod_causa_fim_exercicio" INTEGER,
+    "desc_causa_fim_exercicio" VARCHAR(255),
+    PRIMARY KEY ("id_parlamentar_voz", "id_legislatura")
+);
