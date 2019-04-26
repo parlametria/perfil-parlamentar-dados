0863bbdf13546af8899752b8dffa64847b0bbf6d
Hadrizia <hadrizia@gmail.com>
2019-04-26 11:07:33 -0300

    Modifica referência de parlamentar em respostas

--- bd/analyzer_data_bd.R
+++ bd/analyzer_data_bd.R
@@ -35,17 +35,26 @@ carrega_respostas <- function(data_path = here::here("crawler/raw_data/respostas
 #' @title Processa respostas dos candidatos
 #' @description Processa os dados de respostas dos candidatos (extraídos do monkey) e retorna no formato correto para o banco de dados
 #' @param res_data_path Caminho para o arquivo de respostas sem tratamento.
+#' @param parlamentares_path Caminho para o arquivo de parlamentares
 #' @return Dataframe com id, resposta, cpf, pergunta_id 
-processa_respostas <- function(res_data_path = here::here("crawler/raw_data/respostas.csv")) {
+processa_respostas <- function(res_data_path = here::here("crawler/raw_data/respostas.csv"),
+                               parlamentares_path = here::here("crawler/raw_data/parlamentares.csv")) {
   library(tidyverse)
   library(here)
   
   respostas <- carrega_respostas(res_data_path)
-  
+  parlamentares <- processa_parlamentares(parlamentares_path) %>%
+    dplyr::select(id_parlamentar_voz, cpf)
+
   respostas_alt <- respostas %>% 
+    unique() %>% ## linhas exatamente iguais são eliminadas
+    dplyr::group_by(cpf) %>% 
+    dplyr::mutate(last_date_modified = max(date_modified)) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::filter(date_modified == last_date_modified) %>% ## observações com a última data de atualização são utilizadas
+    dplyr::distinct(cpf, .keep_all = TRUE) %>%
     dplyr::select(cpf, dplyr::starts_with("respostas.")) %>% 
     dplyr::select(-c(respostas.129411238, respostas.129520614, respostas.129521027)) %>% 
-    
     tidyr::gather(key = "pergunta_id", 
                   value = "resposta", 
                   dplyr::starts_with("respostas.")) %>% 
@@ -53,8 +62,8 @@ processa_respostas <- function(res_data_path = here::here("crawler/raw_data/resp
     dplyr::mutate(pergunta_id = as.numeric(pergunta_id)) %>% 
     dplyr::mutate(resposta = dplyr::if_else(is.na(resposta), 0, as.numeric(resposta))) %>% 
     tibble::rowid_to_column(var = "id") %>% 
-    
-    dplyr::select(id, resposta, cpf, pergunta_id)
+    dplyr::inner_join(parlamentares, by="cpf") %>% 
+    dplyr::select(id, resposta, id_parlamentar_voz, pergunta_id)
   
   return(respostas_alt)
 }
