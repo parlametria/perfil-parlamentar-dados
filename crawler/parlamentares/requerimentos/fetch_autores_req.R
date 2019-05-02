4160fdaccba98398260157b6d9e6ec4ba4c33551
Hadrizia <hadrizia@gmail.com>
2019-05-02 17:01:42 -0300

    Adiciona função que pega autores de requerimentos a partir de 01/01/2019

--- crawler/parlamentares/requerimentos/fetch_autores_req.R
+++ crawler/parlamentares/requerimentos/fetch_autores_req.R
@@ -25,29 +25,44 @@ get_uri_autores <- function(uri_autores, id_req) {
   }
 }
 
-fetch_autores_req <- function(prop_id, casa) {
+fetch_autores_req <- function(prop_id, casa, initial_date = lubridate::ymd_hm("2019-01-01 00:00")) {
   paste0("Baixando informações dos autores dos requerimentos da proposição ", prop_id, " na casa ", casa, "...") %>% 
     print()
   if (casa == 'camara') {
-    reqs <- rcongresso::fetch_related_requerimentos_camara(prop_id, TRUE) %>% 
-      dplyr::filter(!is.na(uri_autores))
+    reqs <- rcongresso::fetch_related_requerimentos_camara(prop_id, TRUE) 
     
     if (nrow(reqs) > 0) {
-      df <- purrr::map2_df(reqs$uri_autores, reqs$id_req, ~ get_uri_autores(.x, .y))
-      
-      if (nrow(df) > 0) {
-        return(df %>% 
-                  dplyr::mutate(prop_id = prop_id, casa = casa) %>% 
-                  dplyr::select(prop_id, casa, id_autor = id, nome, id_req))
-      }
+     reqs <- reqs %>% 
+        dplyr::mutate(data_apresentacao = lubridate::ymd_hm(stringr::str_replace(data_apresentacao,"T"," "))) %>% 
+        dplyr::filter(!is.na(uri_autores) & data_apresentacao >= initial_date)
+     if (nrow(reqs) > 0) {
+       df <- purrr::map2_df(reqs$uri_autores, 
+                            reqs$id_req, 
+                            ~ get_uri_autores(.x, .y) %>%
+                              dplyr::left_join(reqs %>% 
+                                                 dplyr::select(id_req, data_apresentacao), 
+                                               by = "id_req"))
+       
+       if (nrow(df) > 0) {
+         return(
+           df %>%
+             dplyr::mutate(prop_id = prop_id, 
+                           casa = casa) %>% 
+             dplyr::select(prop_id, casa, id_autor = id, nome, id_req, data = data_apresentacao)
+         )
+       }
+     }
     }
   }
   
-  return (dplyr::tribble(~ prop_id, ~ casa, ~ id_autor, ~ nome, ~ id_req))
+  return (dplyr::tribble(~ prop_id, ~ casa, ~ id_autor, ~ nome, ~ id_req, ~ data))
 }
 
-fetch_all_autores_req <- function(tabela_votacoes_path = "crawler/raw_data/tabela_votacoes.csv") {
-  tabela_votacoes <- readr::read_csv(tabela_votacoes_path)
+fetch_all_autores_req <- function(tabela_votacoes_path = "crawler/raw_data/tabela_proposicoes_leggo.csv") {
+  tabela_votacoes <- readr::read_csv(tabela_votacoes_path) %>% 
+    dplyr::filter(!is.na(id_camara)) %>% 
+    dplyr::mutate(casa = "camara") %>% 
+    dplyr::select(id_proposicao = id_camara, casa)
   return(purrr::map2_df(tabela_votacoes$id_proposicao, tabela_votacoes$casa, ~ fetch_autores_req(.x, .y)))
 }
 
---
+++ crawler/parlamentares/requerimentos/generate-graph.R
@@ -0,0 +1,35 @@
+library(networkD3)
+library(tidyverse)
+
+generate_graph <- function(df, output_file) {
+  nodes <- df %>% 
+    dplyr::select(nome, id_req, prop_id) %>% 
+    dplyr::group_by(nome) %>% 
+    dplyr::summarise(size = n()) %>% 
+    dplyr::mutate(group = 1, nome = as.factor(nome)) %>% 
+    tibble::rowid_to_column("index") %>% 
+    dplyr::mutate(index = index - 1) %>% 
+    dplyr::select(index, nome, group) %>% 
+    as.data.frame()
+  
+  links <- df %>% full_join(df, by = "id_req") %>%
+    filter(id_autor.x != id_autor.y) %>% 
+    dplyr::select(-c(prop_id.x, casa.x, prop_id.y, casa.y)) %>% 
+    dplyr::group_by(id_autor.x, id_autor.y) %>% 
+    dplyr::summarise(source = first(nome.x), target = first(nome.y), value = n()) %>% 
+    ungroup() %>% 
+    mutate(source = as.factor(source), target = as.factor(target)) %>% 
+    left_join(nodes %>% select(index, nome), by = c("source" = "nome")) %>% 
+    left_join(nodes %>% select(index, nome), by = c("target" = "nome")) %>% 
+    select(source = index.x, target = index.y, value) %>% 
+    arrange(target) %>% 
+    as.data.frame()
+  
+  # Plot
+  fn <- forceNetwork(Links = links, Nodes = nodes,
+                     Source = "source", Target = "target",
+                     Value = "value", NodeID = "nome",
+                     Group = "group", opacity = 0.8, zoom = T)
+  fn %>% 
+    saveNetwork(output_file)
+}
