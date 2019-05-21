bf772ec6c52abe5e556d7633a76d76ffef8d7dc2
Hadrizia <hadrizia@gmail.com>
2019-05-21 11:00:21 -0300

    Refatora código de coautorias

--- reports/coautorias/scripts/generate-graph.R
+++ reports/coautorias/scripts/generate-graph.R
@@ -1,19 +1,20 @@
 library(networkD3)
 library(tidyverse)
 
-generateNodes <- function(df) {
+generate_nodes <- function(df) {
   return(
     df %>%
-      dplyr::distinct(id, nome_eleitoral, sg_partido) %>%
+      dplyr::distinct(id, nome_eleitoral, sg_partido, peso_total_autor) %>%
       tibble::rowid_to_column("index") %>%
       dplyr::mutate(index = index - 1,
                     id = as.character(id),
                     group = as.factor(sg_partido)) %>%
+      dplyr::select(index, id, nome_eleitoral, group, peso_total_autor) %>% 
       as.data.frame()
   )
 }
 
-generateEdges <- function(df, nodes) {
+generate_edges <- function(df, nodes) {
   df <- 
     df %>% 
     ungroup() %>% 
@@ -21,14 +22,26 @@ generateEdges <- function(df, nodes) {
            id.y=as.character(id.y))
   return(
     df %>% 
-      dplyr::group_by(id.x, id.y, peso_arestas) %>% 
-      dplyr::summarise(source = first(id.x), target = first(id.y)) %>% 
+      dplyr::group_by(id.x, id.y) %>% 
+      dplyr::summarise(source = first(id.x), target = first(id.y), peso=sum(peso_arestas)) %>% 
       ungroup() %>% 
       mutate(source = as.factor(source), target = as.factor(target)) %>% 
       left_join(nodes, by = c("source" = "id")) %>% 
       left_join(nodes, by = c("target" = "id")) %>% 
-      select(source = index.x, target = index.y, peso_arestas) %>% 
+      select(source = index.x, target = index.y, peso) %>% 
       arrange(target) %>% 
       as.data.frame()
   )
+}
+
+generate_graph <- function(nodes, edges) {
+  fn <- forceNetwork(
+    Links = edges, Nodes = nodes,
+    Source = "source", Target = "target",
+    Value = "peso", NodeID = "nome_eleitoral",
+    Group ="group",Nodesize = "peso_total_autor",
+    opacity = 0.8, zoom = T,
+    linkColour = "#808080")
+  
+  return(fn)
 }
