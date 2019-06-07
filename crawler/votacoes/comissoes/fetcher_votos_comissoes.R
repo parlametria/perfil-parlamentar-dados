94a04dbb899db718166632217833acbc294d0266
Gileade Kelvin <gileadekelvin@gmail.com>
2019-06-07 14:22:41 -0300

    Deleta fetcher_votos_comissoes

--- crawler/votacoes/comissoes/fetcher_votos_comissoes.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-
-
-fetcher_votacoes_autor <-  function(id, ano = 2019) {
-  url <- paste0("https://www.camara.leg.br/deputados/", id, "/votacoes-comissoes?ano=", ano)
-  
-  
-}
