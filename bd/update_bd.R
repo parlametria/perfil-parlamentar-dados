c1f8a2517e11612abf674a11bccdd14dab234373
gileadekelvin <gileadekelvin@gmail.com>
2019-06-25 10:08:15 -0300

    Resolve bugs durante o processo de atualização dos dados

--- bd/update_bd.R
+++ bd/update_bd.R
@@ -78,6 +78,48 @@ tryCatch(
   }
 )
 
+tryCatch(
+  {
+    log <- paste0(log, date(), " - Realizando migrações para Lideranças...\n")
+    file = here::here("bd/scripts/migrations/migration_liderancas.sql")
+    system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' < ', file))
+  },
+  error=function(cond) {
+    log_error <- get_log_error(cond, "Um erro ocorreu durante a execução da migração de Composição de lideranças")
+    log <- paste0(log, date(), log_error)
+    stop("A execução foi interrompida", call. = FALSE)
+    return(NA)
+  }
+)
+
+tryCatch(
+  {
+    log <- paste0(log, date(), " - Realizando migrações para Mandatos...\n")
+    file = here::here("bd/scripts/migrations/migration_mandatos.sql")
+    system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' < ', file))
+  },
+  error=function(cond) {
+    log_error <- get_log_error(cond, "Um erro ocorreu durante a execução da migração de Mandatos")
+    log <- paste0(log, date(), log_error)
+    stop("A execução foi interrompida", call. = FALSE)
+    return(NA)
+  }
+)
+
+tryCatch(
+  {
+    log <- paste0(log, date(), " - Realizando migrações para Aderência...\n")
+    file = here::here("bd/scripts/migrations/migration_aderencia.sql")
+    system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' < ', file))
+  },
+  error=function(cond) {
+    log_error <- get_log_error(cond, "Um erro ocorreu durante a execução da migração de Composição de Aderência")
+    log <- paste0(log, date(), log_error)
+    stop("A execução foi interrompida", call. = FALSE)
+    return(NA)
+  }
+)
+
 send_log_to_bot(log)
 success <- "As migrações foram realizadas com sucesso!"
 send_log_to_bot(success)
