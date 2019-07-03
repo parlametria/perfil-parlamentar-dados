5ef25bdbf50366cc4ee613a9667d3cfc048ff569
gileadekelvin <gileadekelvin@gmail.com>
2019-07-03 17:28:55 -0300

    Atualiza dados e conserta ids das proposições do questionário

--- crawler/votacoes/analyzer_votos.R
+++ crawler/votacoes/analyzer_votos.R
@@ -42,19 +42,24 @@ enumera_tipos_objetivos_votacao <- function(df) {
 #' @return Dataframe contendo id da votação, id e voto dos deputados que participaram de cada votação
 #' @examples
 #' votacoes <- fetch_votos_camara(2165578, 8334)
-fetch_votos_camara <- function(id_proposicao, id_votacao, resumo_votacao, objeto_votacao) {
+fetch_votos_camara <- function(id_proposicao, numero_projeto_lei, id_votacao, resumo_votacao, objeto_votacao) {
   library(xml2)
-  proposicoes <- rcongresso::fetch_proposicao_camara(id_proposicao) %>%
-    select(siglaTipo, numero, ano)
   
+  proposicoes <- tibble(
+    siglaTipo = strsplit(numero_projeto_lei, " ")[[1]][1],
+    numero = strsplit(strsplit(numero_projeto_lei, "/")[[1]][1], " ")[[1]][2],
+    ano = strsplit(numero_projeto_lei, "/")[[1]][2]
+  )
+
   url <- paste0("https://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ObterVotacaoProposicao?tipo=",
                 proposicoes$siglaTipo, "&numero=", proposicoes$numero, "&ano=", proposicoes$ano)
   
   print(paste0("Baixando votação da ", proposicoes$siglaTipo, " ", proposicoes$numero, "/", proposicoes$ano))
-
-  xml <- RCurl::getURL(url) %>%
-    read_xml()
-
+  
+  xml <- 
+      RCurl::getURL(url) %>%
+      read_xml()
+  
   votacao <- xml_find_all(xml, ".//Votacao") %>%
     map_df(function(x) {
       list(
@@ -121,13 +126,14 @@ processa_votos_camara <- function(votacoes) {
   
   proposicao_votacao <- votacoes %>% 
     dplyr::filter(!is.na(id_sessao)) %>% 
-    dplyr::select(id_proposicao, id_sessao, resumo, objeto_votacao)
+    dplyr::select(numero_proj_lei, id_proposicao, id_sessao, resumo, objeto_votacao)
 
   votos <- purrr::pmap_dfr(list(proposicao_votacao$id_proposicao, 
-                         proposicao_votacao$id_sessao, 
-                         proposicao_votacao$resumo,
-                         proposicao_votacao$objeto_votacao), 
-                    ~ fetch_votos_camara(..1, ..2, ..3, ..4))
+                                proposicao_votacao$numero_proj_lei,
+                                proposicao_votacao$id_sessao, 
+                                proposicao_votacao$resumo,
+                                proposicao_votacao$objeto_votacao), 
+                    ~ fetch_votos_camara(..1, ..2, ..3, ..4, ..5))
 
   parlamentares_filepath = here::here("crawler/raw_data/parlamentares.csv")
   
@@ -157,6 +163,7 @@ processa_votos_camara <- function(votacoes) {
 #' processa_votos(url)
 processa_votos <- function(url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTMcbeHRm_dqX-i2gNVaCiHMFg6yoIjNl9cHj0VBIlQ5eMX3hoHB8cM8FGOukjfNajWDtfvfhqxjji7/pub?gid=0&single=true&output=csv") {
   votacoes_lista <- read_csv(url, col_types = cols(id_proposicao = "c")) %>% 
+    filter(status_proposicao == "Ativa") %>% 
     filter(id_sessao != 99999) # remove votacao da PL 6299/2002 (nao possui votacoes em plenário)
   
   votacoes_camara <- votacoes_lista %>% 
