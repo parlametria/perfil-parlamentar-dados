e40b0c9e578d5778b78b308a978e53eb882c3e6e
gileadekelvin <gileadekelvin@gmail.com>
2019-07-24 09:44:12 -0300

    Adiciona funções para recuperar dados de partidos e blocos do Senado Federal

--- crawler/parlamentares/partidos/analyzer_partidos.R
+++ crawler/parlamentares/partidos/analyzer_partidos.R
@@ -1,151 +1,30 @@
-#' @title Recupera informações de um partido político
-#' @description Recebe um id de partido e retorna os dados detalhados sobre ele
-#' @param Id do partido
-#' @return Dataframe de contendo informações sobre partidos
+#' @title Processamento de dados de Partidos e Blocos na Câmara e no Senado
+#' @description Processa dados de partidos e bloco da Câmara dos Deputados e do Senado Federal
+#' @return Dataframe de contendo informações sobre partidos e blocos
 #' @examples
-#' fetch_info_partido(55)
-fetch_info_partido <- function(id) {
-  print(paste0("Baixando informações do partido de id ", id))
-  
-  url <- paste0(
-    "https://dadosabertos.camara.leg.br/api/v2/partidos/", id)
-  
-  info <- tryCatch({
-    data <- (RCurl::getURI(url) %>%
-               jsonlite::fromJSON())$dados
-    
-    data <- data %>%
-      unlist() %>%
-      t() %>%
-      tibble::as_tibble()
-    
-    data <- data %>%
-      dplyr::mutate(id = as.numeric(id),
-                    tipo = "Partido")
-    
-    if (!"status.situacao" %in% names(data)) {
-      data <- data %>%
-        dplyr::mutate(status.situacao = "Ativo")
-    }
-    
-    data <- data %>%
-      dplyr::select(id,
-                    sigla,
-                    tipo,
-                    situacao = status.situacao)
-    
-  }, error = function(e) {
-    return(
-      dplyr::tribble(~ id, ~ sigla, ~ tipo, ~ situacao)
-      )
-  })
-  
-  return(info) 
-}
-
-#' @title Recupera informações dos partidos políticos de uma legislatura
-#' @description Recebe um id de legislatura e retorna os dados dos partidos políticos brasileiros dessa legislatura
-#' @param Id da legislatura
-#' @return Dataframe de partidos políticos de uma legislatura
-#' @examples
-#' fetch_partidos_por_leg(55)
-fetch_partidos_por_leg <- function(legislatura = 56) {
+#' partidos <- processa_partidos_blocos()
+processa_partidos_blocos <- function() {
   library(tidyverse)
-  url <- 
-    paste0("https://dadosabertos.camara.leg.br/api/v2/partidos?idLegislatura=", 
-           legislatura, 
-           "&itens=100")
+  library(here)
+  source(here("crawler/parlamentares/partidos/fetcher_partidos_camara.R"))
+  source(here("crawler/parlamentares/partidos/fetcher_partidos_senado.R"))
   
-  partidos <- tryCatch({
-    data <- (RCurl::getURI(url) %>% 
-      jsonlite::fromJSON())$dados
-    
-    data <- data %>% 
-      dplyr::select(id)
-    
-  }, error = function(e) {
-    return(
-      dplyr::tribble(~ id, ~ sigla, ~ tipo, ~ situacao)
-      )
-  })
+  partidos_camara <- process_partidos_por_leg()
   
-  if (nrow(partidos) > 0) {
-    partidos <- 
-      purrr::map_df(partidos$id, ~ fetch_info_partido(.x))
-  }
+  # partidos_senado <- fetch_partidos_senado() %>% 
+  #   filter(!str_detect(sigla, "PODEMOS"))
+  # 
+  # partidos_senado_filtrado <- partidos_senado %>% 
+  #   filter(!(sigla %in% (partidos_camara %>% pull(sigla))))
+  # 
+  # partidos <- partidos_camara %>% 
+  #   rbind(partidos_senado_filtrado)
+  # 
+  # check_unique_id <- partidos %>% count(id) %>% nrow()
+  # 
+  # if (check_unique_id != (partidos %>% nrow())) {
+  #   stop("IDs repetidos para o dataframe de Partidos")
+  # }
   
-  return(partidos %>% 
-           dplyr::arrange(id))
-  
-}
-
-#' @title Recupera informações dos blocos partidários de uma legislatura ou ativas na Câmara
-#' @description Recebe um id de legislatura e retorna os dados dos blocos partidários brasileiros dessa legislatura, 
-#' ou os que estão ativos, caso não seja passado nada
-#' @param Id da legislatura
-#' @return Dataframe dos blcoos partidários de uma legislatura ou atualmente ativos
-#' @examples
-#' fetch_blocos()
-fetch_blocos <- function(legislatura = NULL) {
-  library(tidyverse)
-  
-  if (!is.null(legislatura)) {
-    url <- paste0("https://dadosabertos.camara.leg.br/api/v2/blocos?idLegislatura=", legislatura, "&itens=100")
-  } else {
-    url <- "https://dadosabertos.camara.leg.br/api/v2/blocos"
-  }
-  
-  blocos <- tryCatch({
-    data <- 
-      (RCurl::getURI(url) %>% 
-         jsonlite::fromJSON())$dados
-    
-    data <- data %>% 
-      select(id, sigla = nome) %>% 
-      mutate(
-        tipo = "Bloco",
-        situacao = "Ativo",
-        id = as.numeric(id))
-    
-  }, error = function(e) {
-    return(
-      dplyr::tribble(~ id, ~ sigla, ~ tipo, ~ situacao)
-    )
-  })
-  
-  return(blocos %>% 
-           dplyr::arrange(id))
-}
-
-
-#' @title Processa informações dos partidos políticos de uma ou mais legislaturas
-#' @description Recebe um conjunto de ids de legislaturas e retorna os dados dos partidos políticos brasileiros dessas legislaturas e governo
-#' @param Ids das legislaturas
-#' @return Dataframe de partidos políticos das legislaturas e governo
-#' @examples
-#' process_partidos_por_leg(55)
-process_partidos_por_leg <- function(legislaturas = c(55, 56)) {
-  library(tidyverse)
-  
-  partidos <- 
-    purrr::map_df(legislaturas, ~ fetch_partidos_por_leg(.x)) %>% 
-    unique()
-  
-  blocos <- fetch_blocos()
-  
-  governo <- 
-    dplyr::tribble(~ id, ~ sigla, ~ tipo, ~ situacao, 
-                   0, "Governo", "Governo", "Ativo",
-                   1, "Maioria", "Maioria", "Ativo",
-                   2, "Minoria", "Minoria","Ativo",
-                   3, "Oposição", "Oposição","Ativo")
-  
-  partidos <- 
-    dplyr::bind_rows(governo, partidos) %>% 
-    dplyr::bind_rows(blocos) %>% 
-    dplyr::mutate(sigla = gsub("\\.", "", sigla)) %>% 
-    dplyr::arrange(id)
-  
-  return(partidos)
-  
+  return(partidos_camara)
 }
---
+++ crawler/parlamentares/partidos/fetcher_partidos_senado.R
@@ -0,0 +1,56 @@
+#' @title Recupera informações dos partidos e blocos presentes no Senado Federal
+#' @description Recupera lista de partidos e blocos que atualmente possuem lideranças no Senado
+#' @return Dataframe de contendo informações sobre partidos e blocos
+#' @examples
+#' partidos_senado <- fetch_partidos_senado()
+fetch_partidos_senado <- function() {
+  library(tidyverse)
+  library(here)
+  
+  url <- "http://legis.senado.leg.br/dadosabertos/plenario/lista/liderancas"
+  
+  tryCatch({
+    xml <- RCurl::getURL(url) %>% xml2::read_xml()
+    
+    bloco_partidos <- xml2::xml_find_all(xml, ".//Lideranca") %>%
+      map_df(function(x) {
+        list(
+          nome_lideranca = xml2::xml_find_first(x, ".//NomeUnidLideranca") %>%
+            xml2::xml_text(),
+          sigla_lideranca = xml2::xml_find_first(x, ".//SiglaUnidLideranca") %>%
+            xml2::xml_text(),
+          cod_lideranca = xml2::xml_find_first(x, ".//CodigoUnidPartd") %>%
+            xml2::xml_text()
+        )
+      })
+    
+    partidos <- xml2::xml_find_all(xml, ".//Lideranca/Partidos/Partido") %>%
+      map_df(function(x) {
+        list(
+          nome_lideranca = xml2::xml_find_first(x, ".//NomeUnidLideranca") %>%
+            xml2::xml_text(),
+          sigla_lideranca = xml2::xml_find_first(x, ".//SiglaUnidLideranca") %>%
+            xml2::xml_text(),
+          cod_lideranca = xml2::xml_find_first(x, ".//CodigoUnidLideranca") %>%
+            xml2::xml_text()
+        )
+      })
+    
+    partidos_senado <- bloco_partidos %>% 
+      rbind(partidos) %>% 
+      mutate(tipo = if_else(str_detect(sigla_lideranca, "Governo|Maioria|Minoria|Bloco"), 
+                            sigla_lideranca,
+                            "Partido")) %>% 
+      mutate(situacao = "Ativo") %>% 
+      mutate(sigla = if_else(str_detect(sigla_lideranca, "Bloco"), 
+                            nome_lideranca, 
+                            sigla_lideranca)) %>% 
+      select(id = cod_lideranca, sigla, tipo, situacao)
+    
+  }, error = function(e) {
+    data <- tribble(~ id, ~ sigla, ~ tipo, ~ situacao)
+    return(data)
+  })
+  
+  return(partidos_senado)
+}
