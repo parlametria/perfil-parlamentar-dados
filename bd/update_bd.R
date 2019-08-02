eea1652eacd6a39dea2614609d8c0b84da816db4
gileadekelvin <gileadekelvin@gmail.com>
2019-08-02 14:23:59 -0300

    Melhora formatação do arquivo de log e corrige timezone

--- bd/update_bd.R
+++ bd/update_bd.R
@@ -1,66 +1,78 @@
 library(tidyverse)
 source(here::here("bd/send_log_to_bot.R"))
-
-log <- ""
+Sys.setenv(TZ='America/Recife')
 
 host <- Sys.getenv("PGHOST")
 user <- Sys.getenv("PGUSER")
 database <- Sys.getenv("PGDATABASE")
 ## password env PGPASSWORD
 
+execute_migration <- function(migration, log_output) {
+  system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', migration,
+                ' -a -b -e >> ', log_output, ' 2>&1'))
+  write_log("=======================================================", log_output)
+}
+
+write_log <- function(message, log_output) {
+  system(paste0('echo ', message, ' >> ', log_output))
+}
+
 log_file <- here::here(paste0("bd/scripts/logs/",  Sys.Date(), "_log.txt"))
+
+write_log(Sys.time(), log_file)
+write_log("=======================================================", log_file)
 
 ## PARTIDOS
 file = here::here("bd/scripts/migrations/migration_partidos.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e > ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 ## TEMAS
 file = here::here("bd/scripts/migrations/migration_temas.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e >> ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 ## PARLAMENTARES
 file = here::here("bd/scripts/migrations/migration_parlamentares.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e >> ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 ## LIDERANCAS
 file = here::here("bd/scripts/migrations/migration_liderancas.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e >> ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 ## MANDATOS
 file = here::here("bd/scripts/migrations/migration_mandatos.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e >> ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 ## ADERENCIA
 file = here::here("bd/scripts/migrations/migration_aderencia.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e >> ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 ## COMISSOES
 file = here::here("bd/scripts/migrations/migration_comissoes.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e >> ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 ## COMPOSICAO COMISSOES
 file = here::here("bd/scripts/migrations/migration_composicao_comissoes.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e >> ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 ## PROPOSICOES
 file = here::here("bd/scripts/migrations/migration_proposicoes.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e >> ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 ## PROPOSICOES_TEMAS
 file = here::here("bd/scripts/migrations/migration_proposicoes_temas.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e >> ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 ## VOTACOES
 file = here::here("bd/scripts/migrations/migration_votacoes.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e >> ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 ## VOTOS
 file = here::here("bd/scripts/migrations/migration_votos.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e >> ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 ## ORIENTACOES
 file = here::here("bd/scripts/migrations/migration_orientacoes.sql")
-system(paste0('psql -h ', host, ' -U ', user, ' -d ', database, ' -f ', file, ' -a -b -e >> ', log_file, ' 2>&1'))
+execute_migration(file, log_file)
 
 if (length(grep("ROLLBACK", readLines(log_file), value = TRUE)) > 0) {
   error <- paste0('Um erro ocorreu durante a execução das migrações. Mais informações em ', log_file)
