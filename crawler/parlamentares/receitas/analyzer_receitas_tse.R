f98f61a7fd7d99e3d2a15cca04063e418d24953f
gileadekelvin <gileadekelvin@gmail.com>
2019-08-06 13:44:19 -0300

    Atualiza relatório de investimento dos partidos com dados corrigidos de receita dos candidatos

--- crawler/parlamentares/receitas/analyzer_receitas_tse.R
+++ crawler/parlamentares/receitas/analyzer_receitas_tse.R
@@ -4,14 +4,24 @@
 #' @return Dataframe
 #' @examples
 #' doacoes <- processa_doacoes_partidarias_tse()
-processa_doacoes_partidarias_tse <- function(datapath = 
-                                               here::here("crawler/parlamentares/receitas/receitas_candidatos_2018_BRASIL.csv")) {
+processa_doacoes_partidarias_tse <- 
+  function(receitas_datapath = here::here("crawler/parlamentares/receitas/receitas_candidatos_2018_BRASIL.csv"),
+           candidatos_datapath = here::here("crawler/parlamentares/receitas/consulta_cand_2018_BRASIL.csv")) {
+    
   library(tidyverse)
-  
-  receitas <- read_delim(datapath, delim = ";", col_types = cols(SQ_CANDIDATO = "c", VR_RECEITA = "c"),
+    
+  candidatos <- read_delim(candidatos_datapath, delim = ";", col_types = cols(SQ_CANDIDATO = "c"),
+                           locale = locale(encoding = 'latin1')) %>% 
+    filter(DS_SITUACAO_CANDIDATURA == "APTO") %>% 
+    filter(DS_DETALHE_SITUACAO_CAND %in% c("DEFERIDO", "DEFERIDO COM RECURSO")) %>% 
+    select(DS_CARGO, SG_UE, SQ_CANDIDATO, NM_CANDIDATO, NR_CPF_CANDIDATO, SG_PARTIDO) %>% 
+    mutate(DS_CARGO = str_to_title(DS_CARGO)) %>% 
+    filter(DS_CARGO %in% c("Deputado Federal", "Senador"))
+    
+  receitas <- read_delim(receitas_datapath, delim = ";", col_types = cols(SQ_CANDIDATO = "c", VR_RECEITA = "c"),
                          locale = locale(encoding = 'latin1')) %>% 
-    select(DS_CARGO, SG_UE, SQ_CANDIDATO, NM_CANDIDATO, NR_CPF_CANDIDATO, SG_PARTIDO, DS_FONTE_RECEITA, DS_ORIGEM_RECEITA,
-           NM_DOADOR, NM_DOADOR_RFB, VR_RECEITA) %>% 
+    select(DS_CARGO, SG_UE, SQ_CANDIDATO, NM_CANDIDATO, NR_CPF_CANDIDATO, SG_PARTIDO, 
+           DS_FONTE_RECEITA, DS_ORIGEM_RECEITA, NM_DOADOR, NM_DOADOR_RFB, VR_RECEITA) %>% 
     mutate(VR_RECEITA = as.numeric(gsub(",", ".", VR_RECEITA)))
   
   receitas_filtradas <- receitas %>% 
@@ -19,15 +29,14 @@ processa_doacoes_partidarias_tse <- function(datapath =
     filter(trimws(DS_ORIGEM_RECEITA, which = "both") == "Recursos de partido político")
   
   receitas_group <- receitas_filtradas %>% 
-    mutate(id_tse = SQ_CANDIDATO) %>% 
-    group_by(id_tse) %>% 
-    summarise(
-      cargo = first(DS_CARGO),
-      uf = first(SG_UE),
-      partido = first(SG_PARTIDO),
-      nome = first(NM_CANDIDATO),
-      cpf = first(NR_CPF_CANDIDATO),
-      total_receita = sum(VR_RECEITA))
+    group_by(SQ_CANDIDATO) %>% 
+    summarise(total_receita = sum(VR_RECEITA))
   
-  return(receitas_group)
+  candidatos_receita <- candidatos %>% 
+    left_join(receitas_group, by = c("SQ_CANDIDATO")) %>% 
+    mutate(total_receita = if_else(is.na(total_receita), 0, total_receita)) %>% 
+    select(id_tse = SQ_CANDIDATO, cargo = DS_CARGO, uf = SG_UE, partido = SG_PARTIDO, nome = NM_CANDIDATO,
+           cpf = NR_CPF_CANDIDATO, total_receita)
+  
+  return(candidatos_receita)
 }
