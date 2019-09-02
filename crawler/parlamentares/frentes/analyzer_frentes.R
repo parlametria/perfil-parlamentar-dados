27e64e1e6e01e4c28af02d57bd92969afbcc479d
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-09-02 08:33:40 -0300

    finaliza scripts de frentes

--- crawler/parlamentares/frentes/analyzer_frentes.R
+++ crawler/parlamentares/frentes/analyzer_frentes.R
@@ -8,3 +8,19 @@ processa_frentes <- function(ids_frentes_df = readr::read_csv(here::here("crawle
   
   return(list(frentes, membros))
 }
+
+#' @title Recupera informações das frentes e de seus membros
+#' @description Retorna dados de frentes do Congresso Nacional e também seus membros
+#' @return Lista com dois Dataframes: frentes e de membros das frentes
+#' @examples
+#' processa_frentes_membros()
+processa_frentes_membros <- function() {
+  library(tidyverse)
+  
+  dados_frentes <- processa_frentes()
+  
+  frentes <- dados_frentes[[1]]
+  membros_frentes <- dados_frentes[[2]]
+  
+  return(list(frentes, membros_frentes))
+}
--- crawler/parlamentares/frentes/export_frente.R
+++ crawler/parlamentares/frentes/export_frente.R
@@ -13,20 +13,30 @@ message("LEIA O README deste diretório")
 message("Use --help para mais informações\n")
 
 option_list = list(
-  make_option(c("-o", "--out"), type="character", default=here::here("crawler/raw_data/frentes/frentes.csv"), 
-              help="nome do arquivo de saída [default= %default]", metavar="character")
-) 
+  
+  make_option(c("-ocom", "--outFrentes"), type="character", default=here::here("crawler/raw_data/frentes/frentes.csv"),
+              help="nome do arquivo de saída para as informações das frentes [default= %default]", metavar="character"),
+  
+  make_option(c("-oc", "--outMembros"), type="character", default=here::here("crawler/raw_data/frentes/membros_frentes.csv"),
+              help="nome do arquivo de saída para as informações de membros das frentes [default= %default]", metavar="character")
+);
 
-opt_parser = OptionParser(option_list=option_list)
-opt = parse_args(opt_parser)
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
 
-saida <- opt$out
+output_frentes = opt$outFrentes
+output_membros <- opt$outMembros
 
 message("Iniciando processamento...")
-message("Baixando dados...")
-frentes <- processa_frentes()
+dados_frentes <- processa_frentes_membros()
 
-message(paste0("Salvando o resultado em ", saida))
-write.csv(parlamentares, saida, row.names = FALSE)
+frentes <- dados_frentes[[1]]
+
+membros <- dados_frentes[[2]]
+
+message(paste0("Salvando o resultado de frentes em: ", output_frentes))
+readr::write_csv(frentes, output_frentes)
+message(paste0("Salvando o resultado da membros das frentes em: ", output_membros))
+readr::write_csv(membros, output_membros)
 
 message("Concluído!")
