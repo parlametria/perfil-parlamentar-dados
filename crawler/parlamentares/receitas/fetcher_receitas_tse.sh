95f5dfb250b354be18844842196d5f5fec26dc9e
Hadrizia <hadrizia@gmail.com>
2019-09-02 14:13:10 -0300

    Conserta erro em script de baixar dados

--- crawler/parlamentares/receitas/fetcher_receitas_tse.sh
+++ crawler/parlamentares/receitas/fetcher_receitas_tse.sh
@@ -16,7 +16,7 @@ done
 for ano in 2014; do 
 	echo "Fetching year $ano"
 	curl -o prestacao_de_contas_eleitorais_candidatos_${ano}.zip http://agencia.tse.jus.br/estatistica/sead/odsele/prestacao_contas/prestacao_final_${ano}.zip
-  unzip -j prestacao_de_contas_eleitorais_candidatos_${ano}.zip receitas_candidatos_2014_brasil.csv -d .
+  unzip -j prestacao_de_contas_eleitorais_candidatos_${ano}.zip receitas_candidatos_2014_brasil.txt -d .
   rm prestacao_de_contas_eleitorais_candidatos_${ano}.zip
   
   curl -o consulta_cand_${ano}.zip http://agencia.tse.jus.br/estatistica/sead/odsele/consulta_cand/consulta_cand_${ano}.zip
