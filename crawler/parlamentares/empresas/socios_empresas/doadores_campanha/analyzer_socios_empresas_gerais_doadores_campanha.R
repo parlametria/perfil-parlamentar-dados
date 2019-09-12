88650ab1cd141e9b99d77bfd6276fbe10dbb4076
Hadrizia <hadrizia@gmail.com>
2019-09-12 16:04:16 -0300

    Adiciona dados de sócios e empresas doadoras em 2014

--- crawler/parlamentares/empresas/socios_empresas/doadores_campanha/analyzer_socios_empresas_gerais_doadores_campanha.R
+++ crawler/parlamentares/empresas/socios_empresas/doadores_campanha/analyzer_socios_empresas_gerais_doadores_campanha.R
@@ -5,25 +5,23 @@
 #' empresa como exportadora ou não
 #' @param empresas_doadores_datapath Caminho para o dataframe de empresas_doadores
 #' @return Dataframe com informações processadas e se a empresa é exportadora ou não.
-#' @example processa_empresas_doadores()
-processa_empresas_doadores <- function(
+#' @example processa_socios_empresas_doadores()
+processa_socios_empresas_doadores <- function(
   empresas_doadores_datapath = here::here("crawler/raw_data/empresas_doadores.csv"),
   parlamentares_datapath = here::here("crawler/raw_data/parlamentares.csv"),
-  empresas_info_datapath = here::here("crawler/raw_data/empresas_info_2018.csv")) {
+  empresas_info_datapath = NULL) {
   
   library(tidyverse)
   
   source(here::here("crawler/parlamentares/empresas/process_empresas_exportadoras.R"))
-  source(here::here("crawler/parlamentares/empresas/fetcher_empresas.R"))
   
   empresas_doadores <- read_csv(empresas_doadores_datapath) %>% 
-    select(id_deputado = id,
-           cnpj,
+    select(id_deputado,
+           cnpj = cnpj_empresa,
            nome_socio,
-           cnpj_cpf_do_socio,
-           percentual_capital_social,
+           cpf_cnpj_socio,
            data_entrada_sociedade,
-           valor_doado = valor_receita) %>% 
+           valor_doado) %>% 
     classifica_empresas_exportacao() 
   
   parlamentares <- read_csv(parlamentares_datapath) %>% 
@@ -38,7 +36,7 @@ processa_empresas_doadores <- function(
   empresas_doadores_parlamentares <- empresas_doadores_parlamentares %>% 
     select(cnpj,
            exportadora,
-           cpf_cnpj_socio = cnpj_cpf_do_socio,
+           cpf_cnpj_socio,
            nome_socio,
            data_entrada_sociedade,
            id_deputado,
@@ -47,10 +45,66 @@ processa_empresas_doadores <- function(
            uf_deputado,
            valor_doado)
   
-  cnpjs <- empresas_doadores %>% distinct(cnpj) 
+  if (is.null(empresas_info_datapath)) {
+    cnpjs <- empresas_doadores %>% distinct(cnpj) 
+    source(here::here("crawler/parlamentares/empresas/fetcher_empresas.R"))
+    
+    empresas_info <- purrr::map_df(cnpjs$cnpj, ~ fetch_dados_empresa_por_cnpj(.x))
+  } else {
+    empresas_info <- read_csv(empresas_info_datapath,
+                              col_types = cols(.default = "c"))
+  }
   
-  empresas_info <- read_csv(empresas_info_datapath,
-                            col_types = cols(.default = "c"))
+  empresas_doadores_parlamentares_res <- empresas_doadores_parlamentares %>% 
+    left_join(empresas_info, by = "cnpj")
+  
+  return(empresas_doadores_parlamentares_res)
+  
+}
+
+#' @title Processa os dados de parlamentares que receberam doações de empresas
+#' sem filtrar pela classe do CNAE
+#' @description A partir de um dataframe com todas as informações dos parlamentares que 
+#' receberam doações de empresas, processamos esses dados e classificamos a 
+#' empresa como exportadora ou não
+#' @param empresas_doadores_datapath Caminho para o dataframe de empresas_doadores
+#' @return Dataframe com informações processadas e se a empresa é exportadora ou não.
+#' @example processa_empresas_doadores()
+processa_empresas_doadores <- function(
+  empresas_doadores_datapath = here::here("crawler/raw_data/somente_empresas_agricolas_2014.csv"),
+  parlamentares_datapath = here::here("crawler/raw_data/parlamentares.csv"),
+  empresas_info_datapath = NULL) {
+  
+  library(tidyverse)
+  
+  source(here::here("crawler/parlamentares/empresas/process_empresas_exportadoras.R"))
+  
+  empresas_doadores <- read_csv(empresas_doadores_datapath) %>% 
+    select(cnpj = cnpj_empresa,
+           id_deputado,
+           valor_doado) %>% 
+    classifica_empresas_exportacao() %>% 
+    distinct()
+  
+  parlamentares <- read_csv(parlamentares_datapath) %>% 
+    select(id, 
+           nome_deputado = nome_eleitoral, 
+           partido_deputado = sg_partido, 
+           uf_deputado = uf)
+  
+  empresas_doadores_parlamentares <- empresas_doadores %>% 
+    left_join(parlamentares, by = c("id_deputado" = "id"))
+  
+  
+  if (is.null(empresas_info_datapath)) {
+    cnpjs <- empresas_doadores %>% distinct(cnpj) 
+    source(here::here("crawler/parlamentares/empresas/fetcher_empresas.R"))
+    
+    empresas_info <- purrr::map_df(cnpjs$cnpj, ~ fetch_dados_empresa_por_cnpj(.x))
+  } else {
+    empresas_info <- read_csv(empresas_info_datapath,
+                              col_types = cols(.default = "c"))
+  }
   
   empresas_doadores_parlamentares_res <- empresas_doadores_parlamentares %>% 
     left_join(empresas_info, by = "cnpj")
