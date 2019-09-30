14910af980b83e579cf34c054f0cb0eaf9ac5ef3
gileadekelvin <gileadekelvin@gmail.com>
2019-09-30 14:04:01 -0300

    Atualiza dados de parlamentares com informações da data de nascimento Cria função para recuperar informações individuais dos parlamentares no módulo parlametria

---
+++ parlametria/processor/get_info_parlamentares.R
@@ -0,0 +1,21 @@
+#' @title Recupera lista de parlamentares em exercício e informações individuais dos mesmos
+#' @description Recupera informações dos deputados e senadores em exercício
+#' @return Dataframe contendo informações dos parlametares (deputados e senadores) em exercício
+#' @examples
+#' parlamentares_exercicio <- get_info_parlamentares_em_exercicio()
+get_info_parlamentares_em_exercicio <- function() {
+  library(tidyverse)
+  library(here)
+  library(lubridate)
+  
+  parlamentares <- read_csv(here::here("crawler/raw_data/parlamentares.csv"), col_types = cols(id = "c"))
+  
+  parlamentares_exercicio <- parlamentares %>% 
+    filter(em_exercicio == 1) %>% 
+    mutate(data = ymd(data_nascimento)) %>% 
+    mutate(idade_completa = interval(start = data_nascimento, end = Sys.Date()) / duration(num = 1, units = "years")) %>% 
+    mutate(idade = floor(idade_completa)) %>% 
+    select(id, casa, nome_eleitoral, sg_partido, uf, genero, idade)
+    
+  return(parlamentares_exercicio)
+}
