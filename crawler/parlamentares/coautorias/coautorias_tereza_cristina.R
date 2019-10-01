31f37feaec7243772aff2f46dfdded4d2fc62ae4
Hadrizia <hadrizia@gmail.com>
2019-10-01 10:12:45 -0300

    Conserta mudanças propostas em revisão

--- crawler/parlamentares/coautorias/coautorias_tereza_cristina.R
+++ crawler/parlamentares/coautorias/coautorias_tereza_cristina.R
@@ -14,7 +14,7 @@ proposicoes <- fetch_proposicoes_por_autor(178901)
 
 relacionadas <- fetch_all_relacionadas(proposicoes$id)
 
-autores <- fetch_all_autores(relacionadas, parlamentares) %>%
+autores <- fetch_all_autores(relacionadas) %>%
   rename(id_req = id, id = id_deputado) %>%
   distinct() %>%
   group_by(id_req) %>%
