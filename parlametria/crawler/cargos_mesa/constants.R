0f5e31dcded292525967ea38dcc4dc2fb9dd511f
gileadekelvin <gileadekelvin@gmail.com>
2019-10-07 13:59:39 -0300

    Adiciona função para recuperar integrantes da Mesa Diretora do Senado

---
+++ parlametria/crawler/cargos_mesa/constants.R
@@ -0,0 +1,23 @@
+## Cargos comuns entre as casas (Câmara e Senado)
+.SECRETARIO_1 = "1º Secretário"
+.SECRETARIO_2 = "2º Secretário"
+.SECRETARIO_3 = "3º Secretário"
+.SECRETARIO_4 = "4º Secretário"
+
+.VICE_PRESIDENTE_1 = "1º Vice-Presidente"
+.VICE_PRESIDENTE_2 = "2º Vice-Presidente"
+
+.PRESIDENTE = "Presidente"
+
+## Cargos Câmara
+.SUPLENTE_SECRETARIO_1 = "1º Suplente de Secretário"
+.SUPLENTE_SECRETARIO_2 = "2º Suplente de Secretário"
+.SUPLENTE_SECRETARIO_3 = "3º Suplente de Secretário"
+.SUPLENTE_SECRETARIO_4 = "4º Suplente de Secretário"
+
+
+## Cargos Senado
+.SUPLENTE_1 = "1º Suplente"
+.SUPLENTE_2 = "2º Suplente"
+.SUPLENTE_3 = "3º Suplente"
+.SUPLENTE_4 = "4º Suplente"
