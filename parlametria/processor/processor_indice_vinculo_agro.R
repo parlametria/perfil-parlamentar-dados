003d68e7d575c3364e6722bc7473dbb43adf7145
gileadekelvin <gileadekelvin@gmail.com>
2019-10-17 15:13:53 -0300

    Inicia atualização do processor de vínculo com o Agro WIP

--- parlametria/processor/processor_indice_vinculo_agro.R
+++ parlametria/processor/processor_indice_vinculo_agro.R
@@ -7,20 +7,23 @@ process_indice_vinculo_agro <- function() {
   library(tidyverse)
   library(here)
   
-  deputados <- read_csv(here("crawler/raw_data/parlamentares.csv"), col_types = cols(id = "c")) %>% 
-    filter(casa == "camara", em_exercicio == 1)
+  parlamentares <- read_csv(here("crawler/raw_data/parlamentares.csv"), col_types = cols(id = "c")) %>% 
+    filter(em_exercicio == 1)
   
-  deputados_id <- deputados %>% 
+  parlamentares_id <- parlamentares %>% 
     select(id)
   
   propriedades_rurais <- read_csv(here("parlametria/raw_data/patrimonio/propriedades_rurais.csv"),
-                                  col_types = cols(id_casa = "c")) %>% 
-    filter(casa == "camara")
+                                  col_types = cols(id_parlamentar = "c")) %>% 
+    mutate(tem_propriedade_rural = if_else(n_propriedades >= 1, 1, 0)) %>% 
+    select(id_parlamentar, casa, tem_propriedade_rural)
   
-  empresas_rurais <- read_csv(here("parlametria/raw_data/empresas/empresas_parlamentares_agricolas.csv"),
-                              col_type = cols(id_deputado = "c")) %>% 
-    group_by(id_deputado) %>% 
-    summarise(n_empresas_agricolas = n())
+  empresas_rurais <- read_csv(here("parlametria/raw_data/empresas/socios_empresas_agricolas_todos_parlamentares.csv"),
+                              col_type = cols(id_parlamentar = "c")) %>% 
+    group_by(id_parlamentar, casa) %>% 
+    summarise(n_empresas_agricolas = n_distinct(cnpj)) %>% 
+    mutate(tem_empresa_agricola = if_else(n_empresas_agricolas >= 1, 1, 0)) %>% 
+    select(id_parlamentar, casa, tem_empresa_agricola)
   
   doacoes_agro <- read_csv(here("parlametria/raw_data/score_ruralistas/indice_vinculo_economico_agro.csv"),
                            col_types = cols(id = "c")) %>% 
@@ -28,13 +31,8 @@ process_indice_vinculo_agro <- function() {
   
   deputados_processed <- deputados_id %>% 
     
-    left_join(propriedades_rurais, by = c("id" = "id_casa")) %>% 
-    mutate(tem_propriedade_rural = if_else(n_propriedades >= 1, 1, 0)) %>% 
-    select(id, tem_propriedade_rural, total_declarado_propriedade_rural = total_declarado) %>%
-    
-    left_join(empresas_rurais, by = c("id" = "id_deputado")) %>% 
-    mutate(tem_empresa_agricola = if_else(n_empresas_agricolas >= 1, 1, 0)) %>% 
-    select(id, tem_propriedade_rural, total_declarado_propriedade_rural, tem_empresa_agricola) %>% 
+    left_join(propriedades_rurais, by = c("id" = "id_parlamentar", "casa")) %>%
+    left_join(empresas_rurais, by = c("id" = "id_parlamentar", "casa")) %>% 
     
     left_join(doacoes_agro, by = "id") %>% 
     select(id, tem_propriedade_rural, total_declarado_propriedade_rural, tem_empresa_agricola, proporcao_doacoes_agro,
