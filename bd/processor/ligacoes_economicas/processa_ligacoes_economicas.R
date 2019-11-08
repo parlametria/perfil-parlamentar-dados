b10449227def0967f17043c06fbbf6dfd567bf71
gileadekelvin <gileadekelvin@gmail.com>
2019-11-08 10:54:12 -0300

    Adiciona id do grupo de atividades econômicas

--- bd/processor/ligacoes_economicas/processa_ligacoes_economicas.R
+++ bd/processor/ligacoes_economicas/processa_ligacoes_economicas.R
@@ -8,6 +8,7 @@ processa_ligacoes_atividades_economicas <- function(
   library(here)
   
   source(here("parlametria/processor/empresas/processor_indice_atividades_economicas.R"))
+  source(here("parlametria/processor/empresas/processa_empresas.R"))
   
   parlamentares <- read_csv(here("crawler/raw_data/parlamentares.csv"), col_types = cols(id = "c")) %>% 
     filter(em_exercicio == 1) %>% 
@@ -15,15 +16,16 @@ processa_ligacoes_atividades_economicas <- function(
   
   parlamentares_ligacoes <- processa_indices_ligacao_atividade_economica()
   
+  grupos_atividades_economicas <- process_atividade_economica()
+  
   parlamentares_alt <- parlamentares_ligacoes %>% 
     inner_join(parlamentares, by = c("id_parlamentar", "casa")) %>% 
     mutate(
       casa_enum = dplyr::if_else(casa == "camara", 1, 2),
       id_parlamentar_voz = paste0(casa_enum, as.character(id_parlamentar))
     ) %>% 
-    select(id_parlamentar_voz, grupo_atividade_economica, total_por_atividade, proporcao_doacao, indice_ligacao_atividade_economica)
-  
-  ## TODO: inserir merge com id do grupo_atividade_economica
+    left_join(grupos_atividades_economicas, by = c("grupo_atividade_economica" = "nome")) %>% 
+    select(id_parlamentar_voz, id_atividade_economica, total_por_atividade, proporcao_doacao, indice_ligacao_atividade_economica)
   
   return(parlamentares_alt)
 }
