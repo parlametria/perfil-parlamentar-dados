59f97e3fd2f86d86eadc886ba69af4c652921318
gileadekelvin <gileadekelvin@gmail.com>
2020-01-28 12:51:17 -0300

    Ajusta mapeamento da sigla para id do partido

--- crawler/parlamentares/partidos/utils_partidos.R
+++ crawler/parlamentares/partidos/utils_partidos.R
@@ -46,6 +46,27 @@ map_sigla_to_id <- function(df,
 }
 
 
+#' @title Mapeia sigla padronizada para sigla usada na tabela de partidos (crawler/raw_data/partidos.csv)
+#' @description Recebe uma string com a sigla padronizada do partido e retorna a sigla correspondente na
+#' tabela de partidos
+#' @param sigla Sigla padronizada do partido (string)
+#' @return Sigla correspondente em crawler/raw_data/partidos.csv
+map_sigla_padronizada_para_sigla <- function(sigla) {
+  library(tidyverse)
+  
+  sigla_clean <- padroniza_string(sigla)
+  
+  sigla_alt <- case_when(
+    str_detect(sigla_clean, "PODEMOS") ~ "PODE",
+    str_detect(sigla_clean, "REPUBLICANOS") ~ "PRB",
+    str_detect(sigla_clean, "BLOCO PP MDB PTB") ~ "PP, MDB, PTB",
+    str_detect(sigla_clean, "BLOCO PARLAMENTAR PSDBPSL") ~ "BLOCO PARLAMENTAR PSDB/PSL",
+    TRUE ~ sigla_clean
+  )
+  
+  return(sigla_alt)
+}
+
 #' @title Mapeia sigla de partido para id
 #' @description Recebe uma string com a sigla do partido e retorna qual o ID deste partido
 #' @param sigla Sigla do partido (string)
@@ -57,12 +78,11 @@ map_sigla_id <- function(sigla_partido) {
   
   partidos <- suppressWarnings(suppressMessages(read_csv(here("crawler/raw_data/partidos.csv"))))
   
-  sigla_padronizada <- padroniza_sigla(sigla_partido)
+  sigla_padronizada <- padroniza_sigla(sigla_partido) %>% 
+    padroniza_string()
   
   id_partido <- partidos %>% 
-    filter(tolower(sigla) == tolower(if_else(tolower(sigla_padronizada) == "podemos", 
-                                             "PODE", 
-                                             sigla_padronizada))) %>%
+    filter(padroniza_string(sigla) == map_sigla_padronizada_para_sigla(sigla_padronizada)) %>%
     pull(id)
   
   if (length(id_partido) == 0) {
