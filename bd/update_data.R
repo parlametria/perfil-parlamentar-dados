1d7ce9bf9173590b596ade0de274830177cf185a
hadrizia <hadrizia@gmail.com>
2020-02-11 10:16:17 -0300

    Adiciona nome do computador aos logs

--- bd/update_data.R
+++ bd/update_data.R
@@ -5,17 +5,18 @@ Sys.setenv(TZ='America/Recife')
 source(here::here("bd/send_log_to_bot.R"))
 
 log <- ""
-send_log_to_bot(paste0(date(), " - Atualização dos dados iniciada"))
+pc_name <- Sys.info()["nodename"]
+send_log_to_bot(paste0(pc_name, " - ", date(), " - Atualização dos dados iniciada"))
 
 tryCatch(
   {
-    log <- paste0(log, date(), " - Executando crawler de Parlamentares...\n")
+    log <- paste0(log, pc_name, " - ", date(), " - Executando crawler de Parlamentares...\n")
     source(here::here("crawler/parlamentares/export_parlamentares.R"))
   },
   error=function(cond) {
     log_error <- get_log_error(cond, "Um erro ocorreu durante a execução do crawler de Parlamentares")
     message(log_error)
-    log <- paste0(log, date(), " ", log_error)
+    log <- paste0(log, pc_name, " - ", date(), " ", log_error)
     send_log_to_bot(log)
     stop("A execução foi interrompida", call. = FALSE)
     return(NA)
@@ -24,13 +25,13 @@ tryCatch(
 
 tryCatch(
   {
-    log <- paste0(log, date(), " - Executando crawler de Comissões...\n")
+    log <- paste0(log, pc_name, " - ", date(), " - Executando crawler de Comissões...\n")
     source(here::here("crawler/parlamentares/comissoes/export_comissoes.R"))
   },
   error=function(cond) {
     log_error <- get_log_error(cond, "Um erro ocorreu durante a execução do crawler de Comissões")
     message(log_error)
-    log <- paste0(log, date(), " ", log_error)
+    log <- paste0(log, pc_name, " - ", date(), " ", log_error)
     send_log_to_bot(log)
     stop("A execução foi interrompida", call. = FALSE)
     return(NA)
@@ -39,13 +40,13 @@ tryCatch(
 
 tryCatch(
   {
-    log <- paste0(log, date(), " - Executando crawler de Lideranças...\n")
+    log <- paste0(log, pc_name, " - ", date(), " - Executando crawler de Lideranças...\n")
     source(here::here("crawler/parlamentares/liderancas/export_liderancas.R"))
   },
   error=function(cond) {
     log_error <- get_log_error(cond, "Um erro ocorreu durante a execução do crawler de Lideranças")
     message(log_error)
-    log <- paste0(log, date(), " ", log_error)
+    log <- paste0(log, pc_name, " - ", date(), " ", log_error)
     send_log_to_bot(log)
     stop("A execução foi interrompida", call. = FALSE)
     return(NA)
@@ -54,13 +55,13 @@ tryCatch(
 
 tryCatch(
   {
-    log <- paste0(log, date(), " - Executando crawler de Mandatos...\n")
+    log <- paste0(log, pc_name, " - ", date(), " - Executando crawler de Mandatos...\n")
     source(here::here("crawler/parlamentares/mandatos/export_mandatos.R"))
   },
   error=function(cond) {
     log_error <- get_log_error(cond, "Um erro ocorreu durante a execução do crawler de Mandatos")
     message(log_error)
-    log <- paste0(log, date(), " ", log_error)
+    log <- paste0(log, pc_name, " - ", date(), " ", log_error)
     send_log_to_bot(log)
     stop("A execução foi interrompida", call. = FALSE)
     return(NA)
@@ -69,13 +70,13 @@ tryCatch(
 
 tryCatch(
   {
-    log <- paste0(log, date(), " - Executando crawler de Votos (posições)...\n")
+    log <- paste0(log, pc_name, " - ", date(), " - Executando crawler de Votos (posições)...\n")
     source(here::here("crawler/votacoes/votos/export_votos_posicoes.R"))
   },
   error=function(cond) {
     log_error <- get_log_error(cond, "Um erro ocorreu durante a execução do crawler de Votos (posições)")
     message(log_error)
-    log <- paste0(log, date(), " ", log_error)
+    log <- paste0(log, pc_name, " - ", date(), " ", log_error)
     send_log_to_bot(log)
     stop("A execução foi interrompida", call. = FALSE)
     return(NA)
@@ -84,13 +85,13 @@ tryCatch(
 
 tryCatch(
   {
-    log <- paste0(log, date(), " - Executando crawler de Votos (aderência)...\n")
+    log <- paste0(log, pc_name, " - ", date(), " - Executando crawler de Votos (aderência)...\n")
     source(here::here("crawler/votacoes/votos/export_votos.R"))
   },
   error=function(cond) {
     log_error <- get_log_error(cond, "Um erro ocorreu durante a execução do crawler de Votos (aderência)")
     message(log_error)
-    log <- paste0(log, date(), " ", log_error)
+    log <- paste0(log, pc_name, " - ", date(), " ", log_error)
     send_log_to_bot(log)
     stop("A execução foi interrompida", call. = FALSE)
     return(NA)
@@ -99,13 +100,13 @@ tryCatch(
 
 tryCatch(
   {
-    log <- paste0(log, date(), " - Executando crawler de Orientações...\n")
+    log <- paste0(log, pc_name, " - ", date(), " - Executando crawler de Orientações...\n")
     source(here::here("crawler/votacoes/orientacoes/export_orientacoes.R"))
   },
   error=function(cond) {
     log_error <- get_log_error(cond, "Um erro ocorreu durante a execução do crawler de Orientações")
     message(log_error)
-    log <- paste0(log, date(), " ", log_error)
+    log <- paste0(log, pc_name, " - ", date(), " ", log_error)
     send_log_to_bot(log)
     stop("A execução foi interrompida", call. = FALSE)
     return(NA)
@@ -114,13 +115,13 @@ tryCatch(
 
 tryCatch(
   {
-    log <- paste0(log, date(), " - Executando crawler de Votações (informações detalhadas)...\n")
+    log <- paste0(log, pc_name, " - ", date(), " - Executando crawler de Votações (informações detalhadas)...\n")
     source(here::here("crawler/votacoes/votacoes_nominais/export_votacoes_info.R"))
   },
   error=function(cond) {
     log_error <- get_log_error(cond, "Um erro ocorreu durante a execução do crawler de Votações (informações detalhadas)")
     message(log_error)
-    log <- paste0(log, date(), " ", log_error)
+    log <- paste0(log, pc_name, " - ", date(), " ", log_error)
     send_log_to_bot(log)
     stop("A execução foi interrompida", call. = FALSE)
     return(NA)
@@ -129,13 +130,13 @@ tryCatch(
 
 tryCatch(
   {
-    log <- paste0(log, date(), " - Executando crawler de Investimento Partidário...\n")
+    log <- paste0(log, pc_name, " - ", date(), " - Executando crawler de Investimento Partidário...\n")
     source(here::here("parlametria/crawler/resumo/investimento_partidario/export_investimento_partidario.R"))
   },
   error=function(cond) {
     log_error <- get_log_error(cond, "Um erro ocorreu durante a execução do crawler de Investimento Partidário")
     message(log_error)
-    log <- paste0(log, date(), " ", log_error)
+    log <- paste0(log, pc_name, " - ", date(), " ", log_error)
     send_log_to_bot(log)
     stop("A execução foi interrompida", call. = FALSE)
     return(NA)
@@ -144,13 +145,13 @@ tryCatch(
 
 tryCatch(
   {
-    log <- paste0(log, date(), " - Executando processador dos dados para o perfil mais...\n")
+    log <- paste0(log, pc_name, " - ", date(), " - Executando processador dos dados para o perfil mais...\n")
     source(here::here("parlametria/processor/export_dados_perfil_mais.R"))
   },
   error=function(cond) {
     log_error <- get_log_error(cond, "Um erro ocorreu durante a execução processador dos dados para o perfil mais")
     message(log_error)
-    log <- paste0(log, date(), " ", log_error)
+    log <- paste0(log, pc_name, " - ", date(), " ", log_error)
     send_log_to_bot(log)
     stop("A execução foi interrompida", call. = FALSE)
     return(NA)
@@ -159,13 +160,28 @@ tryCatch(
 
 tryCatch(
   {
-    log <- paste0(log, date(), " - Executando processamento dos dados para o formato do BD...\n")
+    log <- paste0(log, pc_name, " - ", date(), " - Executando processador dos dados de cargos na mesa diretora...\n")
+    source(here::here("parlametria/crawler/cargos_mesa/export_cargos_mesa.R"))
+  },
+  error=function(cond) {
+    log_error <- get_log_error(cond, "Um erro ocorreu durante a execução processador dos dados de cargos na mesa diretora")
+    message(log_error)
+    log <- paste0(log, pc_name, " - ", date(), " ", log_error)
+    send_log_to_bot(log)
+    stop("A execução foi interrompida", call. = FALSE)
+    return(NA)
+  }
+)
+
+tryCatch(
+  {
+    log <- paste0(log, pc_name, " - ", date(), " - Executando processamento dos dados para o formato do BD...\n")
     source(here::here("bd/export_dados_tratados_bd.R"))
   },
   error=function(cond) {
     log_error <- get_log_error(cond, "Um erro ocorreu durante o processamento dos dados para o formato do BD")
     message(log_error)
-    log <- paste0(log, date(), " ", log_error)
+    log <- paste0(log, pc_name, " - ", date(), " ", log_error)
     send_log_to_bot(log)
     stop("A execução foi interrompida", call. = FALSE)
     return(NA)
