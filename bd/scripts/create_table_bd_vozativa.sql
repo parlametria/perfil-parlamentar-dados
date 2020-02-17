130b72a8a66d110abd4b6ad5808d4cabcd1919f2
gileadekelvin <gileadekelvin@gmail.com>
2020-02-17 08:50:25 -0300

    Atualiza migrations de mandatos

--- bd/scripts/create_table_bd_vozativa.sql
+++ bd/scripts/create_table_bd_vozativa.sql
@@ -132,12 +132,15 @@ CREATE TABLE IF NOT EXISTS "composicao_comissoes" (
 CREATE TABLE IF NOT EXISTS "mandatos" (
     "id_mandato_voz" VARCHAR(40),
     "id_parlamentar_voz" VARCHAR(40) REFERENCES "parlamentares" ("id_parlamentar_voz") ON DELETE CASCADE ON UPDATE CASCADE,
-    "id_legislatura" INTEGER,
-    "data_inicio" DATE,
-    "data_fim" DATE,
-    "situacao" VARCHAR(255),
-    "cod_causa_fim_exercicio" INTEGER,
-    "desc_causa_fim_exercicio" VARCHAR(255),
+    "ano_eleicao" INTEGER,
+    "num_turno" INTEGER,
+    "cargo" VARCHAR(20),
+    "unidade_eleitoral" VARCHAR(30),
+    "uf_eleitoral" VARCHAR(2),
+    "situacao_candidatura" VARCHAR(30),
+    "situacao_totalizacao_turno" VARCHAR(50),
+    "id_partido" INTEGER REFERENCES "partidos" ("id_partido") ON DELETE SET NULL ON UPDATE CASCADE,
+    "votos" INTEGER,
     PRIMARY KEY ("id_mandato_voz")
 );
 
--- bd/scripts/migrations/migration_mandatos.sql
+++ bd/scripts/migrations/migration_mandatos.sql
@@ -4,21 +4,23 @@ CREATE TEMP TABLE temp_mandatos AS SELECT * FROM mandatos LIMIT 0;
 
 \copy temp_mandatos FROM './data/mandatos.csv' WITH NULL AS 'NA' DELIMITER ',' CSV HEADER;
 
-INSERT INTO mandatos (id_mandato_voz, id_parlamentar_voz, id_legislatura, data_inicio, 
-  data_fim, situacao, cod_causa_fim_exercicio, desc_causa_fim_exercicio)
-
-SELECT id_mandato_voz, id_parlamentar_voz, id_legislatura, data_inicio, 
-data_fim, situacao, cod_causa_fim_exercicio, desc_causa_fim_exercicio
+INSERT INTO mandatos
+SELECT *
 FROM temp_mandatos
 ON CONFLICT (id_mandato_voz) 
 DO
   UPDATE
     SET 
-      data_inicio = EXCLUDED.data_inicio,
-      data_fim = EXCLUDED.data_fim,
-      situacao = EXCLUDED.situacao,
-      cod_causa_fim_exercicio = EXCLUDED.cod_causa_fim_exercicio,
-      desc_causa_fim_exercicio = EXCLUDED.desc_causa_fim_exercicio;
+      id_parlamentar_voz = EXCLUDED.id_parlamentar_voz,
+      ano_eleicao = EXCLUDED.ano_eleicao,
+      num_turno = EXCLUDED.num_turno,
+      cargo = EXCLUDED.cargo,
+      unidade_eleitoral = EXCLUDED.unidade_eleitoral,
+      uf_eleitoral = EXCLUDED.uf_eleitoral,
+      situacao_candidatura = EXCLUDED.situacao_candidatura,
+      situacao_totalizacao_turno = EXCLUDED.situacao_totalizacao_turno,
+      id_partido = EXCLUDED.id_partido,
+      votos = EXCLUDED.votos;
       
 DELETE FROM mandatos
 WHERE (id_mandato_voz) NOT IN
