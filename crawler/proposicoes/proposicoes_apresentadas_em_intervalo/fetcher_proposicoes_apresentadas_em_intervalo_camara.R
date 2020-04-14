f7d4654ab5907c4c7ade6ca342b1a11e6593463b
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-04-14 18:09:35 -0300

    Adiciona distinct e formata arquivo

--- crawler/proposicoes/proposicoes_apresentadas_em_intervalo/fetcher_proposicoes_apresentadas_em_intervalo_camara.R
+++ crawler/proposicoes/proposicoes_apresentadas_em_intervalo/fetcher_proposicoes_apresentadas_em_intervalo_camara.R
@@ -1,4 +1,4 @@
-#' @title Recupera dados das proposições que foram apresentadas 
+#' @title Recupera dados das proposições que foram apresentadas
 #' em uma página da API, em dado intervalo de datas e o número de itens.
 #' @description Recebe um número de página, uma data inicial,
 #' uma data final e a quantidade de itens a retornar de
@@ -46,7 +46,7 @@ fetch_proposicoes_por_pagina <-
         select(id, sisgla_tipo = siglaTipo, numero, ano)
       
     }, error = function(e) {
-      return(tribble( ~ id, ~ sigla_tipo, ~ numero, ~ ano))
+      return(tribble(~ id, ~ sigla_tipo, ~ numero, ~ ano))
     })
     
     return(proposicoes)
@@ -54,7 +54,7 @@ fetch_proposicoes_por_pagina <-
 
 #' @title Recupera dados das proposições que foram apresentadas em
 #' um intervalo de datas e o número de itens por página.
-#' @description Recebe uma data inicial, uma data final e a 
+#' @description Recebe uma data inicial, uma data final e a
 #' quantidade de itens a retornar de proposições apresentadas por
 #' página. (Máximo de 100)
 #' @param data_inicial Data inicial do intervalo (formato AAAA-MM-DD)
@@ -62,40 +62,44 @@ fetch_proposicoes_por_pagina <-
 #' @param itens número de itens que se deseja retornar
 #' @return Dataframe com lista de proposições apresentadas em um intervalo
 #' de datas com o número de itens por página selecionado.
-fetcher_proposicoes_em_intervalo_camara <- function(data_inicial = "2020-03-11",
-                                                    data_final = Sys.Date(),
-                                                    itens = 100) {
-  library(tidyverse)
-  
-  url <-
-    paste0(
-      "https://dadosabertos.camara.leg.br/api/v2/proposicoes?dataApresentacaoInicio=",
-      data_inicial,
-      "&dataApresentacaoFim=",
-      data_final,
-      "&itens=",
-      itens
-    )
-  
-  proposicoes <- tryCatch({
-    links <- (RCurl::getURI(url) %>%
-                jsonlite::fromJSON())$links
+fetcher_proposicoes_em_intervalo_camara <-
+  function(data_inicial = "2020-03-11",
+           data_final = Sys.Date(),
+           itens = 100) {
+    library(tidyverse)
     
-    num_pags <- links %>%
-      filter(rel == "last") %>%
-      pull(href) %>%
-      str_extract("&pagina=[\\d]*&") %>%
-      str_extract("[\\d]+")
+    url <-
+      paste0(
+        "https://dadosabertos.camara.leg.br/api/v2/proposicoes?dataApresentacaoInicio=",
+        data_inicial,
+        "&dataApresentacaoFim=",
+        data_final,
+        "&itens=",
+        itens
+      )
     
-    paginas <- seq(1, num_pags)
+    proposicoes <- tryCatch({
+      links <- (RCurl::getURI(url) %>%
+                  jsonlite::fromJSON())$links
+      
+      num_pags <- links %>%
+        filter(rel == "last") %>%
+        pull(href) %>%
+        str_extract("&pagina=[\\d]*&") %>%
+        str_extract("[\\d]+")
+      
+      paginas <- seq(1, num_pags)
+      
+      data <-
+        purrr::map_df(paginas,
+                      ~ fetch_proposicoes_por_pagina(.x, data_inicial, data_final, itens))
+      
+      data <- data %>%
+        dplyr::distinct(id, .keep_all = TRUE)
+      
+    }, error = function(e) {
+      return(tribble(~ id, ~ sigla_tipo, ~ numero, ~ ano))
+    })
     
-    data <-
-      purrr::map_df(paginas,
-                    ~ fetch_proposicoes_por_pagina(.x, data_inicial, data_final, itens))
-    
-  }, error = function(e) {
-    return(tribble( ~ id, ~ sigla_tipo, ~ numero, ~ ano))
-  })
-  
-  return(proposicoes)
-}
+    return(proposicoes)
+  }
