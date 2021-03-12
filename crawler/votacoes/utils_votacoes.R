44f7b893a8f340db91836fe9419b3c3adeb8cf79
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2021-03-12 17:07:36 -0300

    Conserta confusão em mapeamento de nome de partido para id

--- crawler/votacoes/utils_votacoes.R
+++ crawler/votacoes/utils_votacoes.R
@@ -28,18 +28,18 @@ padroniza_sigla <- function(sigla) {
   sigla = toupper(sigla)
   
   sigla_padronizada <- case_when(
-    str_detect(tolower(sigla), "ptdob") ~ "AVANTE",
-    str_detect(tolower(sigla), "pcdob") ~ "PCdoB",
-    str_detect(tolower(sigla), "ptn") ~ "PODEMOS",
-    str_detect(tolower(sigla), "pps") ~ "CIDADANIA",
-    str_detect(tolower(sigla), "pmdb") ~ "MDB",
-    str_detect(tolower(sigla), "patri") ~ "PATRIOTA",
-    str_detect(tolower(sigla), "pc do b") ~ "PCdoB",
-    tolower(sigla) == "pr" ~ "PL",
-    str_detect(sigla, "SOLID.*") ~ "SOLIDARIEDADE",
-    str_detect(sigla, "PODE.*") ~ "PODEMOS",
-    str_detect(sigla, "GOV.") ~ "GOVERNO",
-    str_detect(sigla, "PHS.*") ~ "PHS",
+    str_detect(tolower(sigla), "^ptdob") ~ "AVANTE",
+    str_detect(tolower(sigla), "^pcdob") ~ "PCdoB",
+    str_detect(tolower(sigla), "^ptn") ~ "PODEMOS",
+    str_detect(tolower(sigla), "^pps") ~ "CIDADANIA",
+    str_detect(tolower(sigla), "^pmdb") ~ "MDB",
+    str_detect(tolower(sigla), "^patri") ~ "PATRIOTA",
+    str_detect(tolower(sigla), "^pc do b") ~ "PCdoB",
+    tolower(sigla) == "^pr" ~ "PL",
+    str_detect(sigla, "^SOLID.*") ~ "SOLIDARIEDADE",
+    str_detect(sigla, "^PODE.*") ~ "PODEMOS",
+    str_detect(sigla, "^GOV.") ~ "GOVERNO",
+    str_detect(sigla, "^PHS.*") ~ "PHS",
     TRUE ~ sigla
   ) %>%
     stringr::str_replace("REPR.", "") %>% 
